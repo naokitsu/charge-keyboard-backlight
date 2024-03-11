595d86cd95591769e51b6b15686de5ca036b25a8
Naokitsu <naokitsu@tutanota.com>
2024-03-11 06:11:23 +0400

    set the color of the keyboard

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use crate::config::{Color, Config, Payload, Mode};
+
 mod config;
 
 fn main() {
-    println!("Hello, world!");
+    let mut config = Config::new();
+    config.load( Payload { mode: Mode::Static(Color { red: 125, green: 0, blue: 30 }), save: true }).unwrap();
 }
